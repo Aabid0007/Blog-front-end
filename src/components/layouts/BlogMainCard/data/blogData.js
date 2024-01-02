cd07e338c2d58e29db0b7cfdd2afd9be2027c5de
Aabid <zainulaabid0007@gmail.com>
2024-01-02 18:17:56 +0530

    test

--- src/components/layouts/BlogMainCard/data/blogData.js
+++ src/components/layouts/BlogMainCard/data/blogData.js
@@ -22,7 +22,7 @@ const blogs = [
                 ultricies mi non congue ullam corper. Praesent tincidunt sed  tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.
                 ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non 
                 congue ullam corper. Praesent tincidunt sed  tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.`
-    },7
+    },
     
 ];
 
